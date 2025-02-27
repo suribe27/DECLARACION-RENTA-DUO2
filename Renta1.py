0195602033855d510898ba054b00777902a3c1ca
Migue <sapotico1234@gmail.com>
2025-02-26 19:11:24 -0500

    Actualiazada logica de renta

--- Renta1.py
+++ Renta1.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+VALOR_UVT: int = 49_799
 ingresos_brutos_anuales : float = 80_000_000
 aportes_salud_pension : float = 4_800_000
 numero_dependientes : float = 6_000_000
@@ -13,16 +14,34 @@ print(obtener_base_gravable(ingresos_brutos_anuales, aportes_salud_pension, nume
 
 
 def obtener_base_gravable_en_uvt(base_gravable):
-    base_gravable_en_uvt = base_gravable / 49_799 #Valor de conversión para pasar a UVT
+    base_gravable_en_uvt = base_gravable / VALOR_UVT
 
-    return base_gravable_en_uvt
+    return round(base_gravable_en_uvt)
 
 base_gravable_en_uvt = obtener_base_gravable_en_uvt(base_gravable)
 print(obtener_base_gravable_en_uvt(base_gravable))
 
 def obtener_impuesto():
-    impuesto: float = ((base_gravable_en_uvt - 1_090) * 0.19) * 49_799
-    return impuesto
+    
+    if base_gravable_en_uvt > 1090 and base_gravable_en_uvt < 1700:
+        impuesto: float = ((base_gravable_en_uvt - 1_090) * 0.19) * VALOR_UVT
+
+    elif base_gravable_en_uvt > 1700 and base_gravable_en_uvt < 4100:
+        impuesto: float = ((base_gravable_en_uvt - 1_700) * 0.28 + 116) * VALOR_UVT
+
+    elif base_gravable_en_uvt > 4100 and base_gravable_en_uvt < 8670:
+        impuesto: float = ((base_gravable_en_uvt - 4100) * 0.33 + 788) * VALOR_UVT
+    
+    elif base_gravable_en_uvt > 8670 and base_gravable_en_uvt < 18970:
+        impuesto: float = ((base_gravable_en_uvt - 8670) * 0.35 + 2296) * VALOR_UVT
+
+    elif base_gravable_en_uvt > 18970 and base_gravable_en_uvt < 31000:
+        impuesto: float = ((base_gravable_en_uvt - 18970) * 0.37 + 5901) * VALOR_UVT
+
+    elif base_gravable_en_uvt > 31000:
+        impuesto: float = ((base_gravable_en_uvt - 31000) * 0.39 + 10352) * VALOR_UVT
+    
+    return round(impuesto)
 
 obtener_impuesto()
 print(obtener_impuesto())
