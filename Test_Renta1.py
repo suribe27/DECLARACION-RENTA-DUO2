47554f67029a48e05fd715ec6987e7b1b04dd672
Migue <sapotico1234@gmail.com>
2025-02-27 17:50:29 -0500

    main en pruebas

--- Test_Renta1.py
+++ Test_Renta1.py
@@ -3,7 +3,7 @@ import unittest
 import LogicaRenta
 
 class RentaTest(unittest.TestCase):
-    def testbaseGravable(self):
+    def testbaseGravable_1(self):
 
         ingresos_brutos_anuales = 80_000_000
         aportes_salud_pension = 4_800_000
@@ -16,7 +16,7 @@ class RentaTest(unittest.TestCase):
         self.assertAlmostEqual(expected, result, 0)
 
 
-    def testBaseGravableUVT(self):
+    def testBaseGravableUVT_1(self):
         
         base_gravable = 69_200_000
 
@@ -25,13 +25,18 @@ class RentaTest(unittest.TestCase):
         expected = 1_390
 
         self.assertAlmostEqual(expected, result, 0)
-        pass
-
-    def testImpuestos(self):
         
-        result = LogicaRenta.obtener_impuesto()
+
+    def testImpuestos_1(self):
+        
+        base_gravable_en_uvt = 1_390
+        
+        result = LogicaRenta.obtener_impuesto(base_gravable_en_uvt)
 
         expected = 2_838_543
 
         self.assertAlmostEqual(expected, result, 0)
-        pass
+        
+
+if __name__ == '__main__':
+    unittest.main()
